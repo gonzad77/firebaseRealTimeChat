7f7ff8e8533be8f7d6ec49889640447f42841b81
Gonzalo Di Giovanni <gonza.digiovanni@gmail.com>
2016-08-05 17:54:58 -0300

    Se crean las vistas y lso controllers a usar vacios

--- realTimeChat/www/js/app.js
+++ realTimeChat/www/js/app.js
@@ -3,22 +3,123 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic','firebase','starter.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
+.run(function($ionicPlatform, $rootScope, $state, AuthService) {
+  $ionicPlatform.ready(function(){
+    AuthService.userIsLoggedIn().then(function(response)
+    {
+      if(response === true)
+      {
+        $state.go('app.user');
+      }
+      else
+      {
+        $state.go('auth.login');
+      }
+    });
+
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
+
+  // UI Router Authentication Check
+  $rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams){
+    if (toState.data.authenticate)
+    {
+      AuthService.userIsLoggedIn().then(function(response)
+      {
+        if(response === false)
+        {
+          event.preventDefault();
+          $state.go('auth.login');
+        }
+      });
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the auth section
+  .state('auth', {
+    url: '/auth',
+    abstract: true
+  })
+
+  .state('auth.login', {
+    url: '/login',
+    views: {
+      'main-view@': {
+        templateUrl: 'views/auth/login.html',
+        controller: 'LogInCtrl'
+      }
+    },
+    data: {
+      authenticate: false
+    }
+  })
+
+  .state('auth.signup', {
+    url: '/signup',
+    views: {
+      'main-view@': {
+        templateUrl: 'views/auth/signup.html',
+        controller: 'SignUpCtrl'
+      }
+    },
+    data: {
+      authenticate: false
+    }
+  })
+
+  .state('app', {
+    url: '/app',
+    abstract: true
+  })
+
+  .state('app.chat', {
+    url: '/chat',
+    views: {
+      'main-view@': {
+        templateUrl: 'views/app/chat.html',
+        controller: 'ChatCtrl'
+      }
+    },
+    data: {
+      authenticate: true
+    }
+  })
+
+  .state('app.chatrooms', {
+    url: '/chatrooms',
+    views: {
+      'main-view@': {
+        templateUrl: 'views/app/chatrooms.html',
+        controller: 'ChatRoomsCtrl'
+      }
+    },
+    data: {
+      authenticate: true
+    }
+  })
+
+  ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/chatrooms');
 })
---
+++ realTimeChat/www/js/controllers.js
@@ -0,0 +1,17 @@
+angular.module('starter.controllers', [])
+
+.controller('LogInCtrl', function($scope, $state, $ionicLoading) {
+
+}
+
+.controller('SignUpCtrl', function($scope, $state, $ionicLoading) {
+
+}
+
+.controller('ChatCtrl', function($scope, $state, $ionicLoading) {
+
+}
+
+.controller('ChatRoomsCtrl', function($scope, $state, $ionicLoading) {
+
+}
