3d903fa502b94ad0122b1779b480ac3040ba0550
Gonzalo Di Giovanni <gonza.digiovanni@gmail.com>
2016-08-09 15:18:59 -0300

    Se agrega funcionalidad de login and signup con firebase

--- realTimeChat/www/js/app.js
+++ realTimeChat/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic','firebase','starter.controllers'])
+angular.module('starter', ['ionic','firebase','starter.controllers','starter.services'])
 
 .run(function($ionicPlatform, $rootScope, $state, AuthService) {
   $ionicPlatform.ready(function(){
@@ -11,7 +11,7 @@ angular.module('starter', ['ionic','firebase','starter.controllers'])
     {
       if(response === true)
       {
-        $state.go('app.user');
+        $state.go('app.chatrooms');
       }
       else
       {
--- realTimeChat/www/js/controllers.js
+++ realTimeChat/www/js/controllers.js
@@ -1,17 +1,46 @@
 angular.module('starter.controllers', [])
 
-.controller('LogInCtrl', function($scope, $state, $ionicLoading) {
+.controller('LogInCtrl', function($scope, $state, AuthService, $ionicLoading) {
+  $scope.login = function(user){
+   $ionicLoading.show({
+     template: 'Logging in ...'
+   });
+   AuthService.doLogin(user)
+    .then(function(user){
+     // success
+     $state.go('app.chatrooms');
+     $ionicLoading.hide();
+   },function(err){
+     // error
+     $scope.errors = err;
+     $ionicLoading.hide();
+   });
+ };
+})
 
-}
+.controller('SignUpCtrl', function($scope, $state, AuthService, $ionicLoading) {
+  $scope.signup = function(user){
+      $ionicLoading.show({
+        template: 'Signing up ...'
+      });
 
-.controller('SignUpCtrl', function($scope, $state, $ionicLoading) {
-
-}
+      AuthService.doSignup(user)
+      .then(function(user){
+        // success
+        $state.go('app.chatrooms');
+        $ionicLoading.hide();
+      },function(err){
+        // error
+        $scope.errors = err;
+        $ionicLoading.hide();
+      });
+    };
+  })
 
 .controller('ChatCtrl', function($scope, $state, $ionicLoading) {
 
-}
+})
 
 .controller('ChatRoomsCtrl', function($scope, $state, $ionicLoading) {
 
-}
+})
